f383f9df492dc1391cb0936f3782d77043463dd0
AanchalCh <aanchal.chaurasia99@gmail.com>
2022-03-06 16:31:08 +0530

    Changing winning condition

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ btnHold.addEventListener('click', function(){
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
         //2. Check if player's score is >=100
         // Finish the game
-        if(scores[activePlayer]>=20) {
+        if(scores[activePlayer]>=100) {
             playing = false
             diceEl.classList.add('hidden')
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
